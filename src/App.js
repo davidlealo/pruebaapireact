551690c906a0155efdbdff99550bc85724c83afd
davidlealo <davidlealo@gmail.com>
2022-11-16 08:50:13 -0300

    Filtro funcionando

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ function App() {
   return (
     <div className="App ">
         <MiApi className="personajes"/>
-        <div className='imagenRandM'>
-        <iframe src="https://giphy.com/embed/l378BzHA5FwWFXVSg" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/adultswim-l378BzHA5FwWFXVSg">via GIPHY</a></p>
-        </div>
     </div>
   );
 }
--- src/components/MiApi.jsx
+++ src/components/MiApi.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react"
 
 function MiApi() {
         const [state, setState] = useState([])
+        const [search, setSearch] = useState('')
         const llamarAPI = async () =>{
             const response = await fetch('https://rickandmortyapi.com/api/character')
             const data = await response.json()
@@ -12,14 +13,25 @@ function MiApi() {
             llamarAPI()
         },[])
 
+        const searcher = (e) =>{
+            setSearch(e.target.value)
+        }
     
-    
+        let result = []
+        if(search === ""){
+            result = state
+        }else{
+            result = state.filter((dato)=>
+                dato.name.toLowerCase().includes(search.toLowerCase()))
+            }
+        
 
     return (
         <div>
             <h1>Personajes de Rick and Morty</h1>
+            <input value ={search} onChange={searcher}/>
             <ul>
-        {state.map(estado => <li key={estado.id}><strong>Nombre: </strong>{estado.name} -- <strong>Estado: </strong>{estado.status} -- <strong>Especie: </strong>{estado.species}</li>)}
+        {result.map(estado => <li key={estado.id}><strong>Nombre: </strong>{estado.name} -- <strong>Estado: </strong>{estado.status} -- <strong>Especie: </strong>{estado.species}</li>)}
       </ul>
         </div>
     );
