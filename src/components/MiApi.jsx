42936365303a405fd6a3558a954bdae45d042a17
davidlealo <davidlealo@gmail.com>
2022-11-15 22:38:46 -0300

    Hice funcionar la api con use effect

--- src/components/MiApi.jsx
+++ src/components/MiApi.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from "react"
 
 function MiApi() {
-  return (
-    <div>MiApi</div>
-  )
+        const [state, setState] = useState([])
+        const llamarAPI = async () =>{
+            const response = await fetch('https://rickandmortyapi.com/api/character')
+            const data = await response.json()
+            setState(data.results)
+        }
+
+        useEffect(()=>{
+            llamarAPI()
+            console.log(state)
+        },[])
+
+    
+    
+
+    return (
+        <div>
+            <h1>Hola Mundo</h1>
+            <ul>
+        {state.map(estado => <li key={estado.id}><strong>Nombre: </strong>{estado.name} -- <strong>Estado: </strong>{estado.status} -- <strong>Especie: </strong>{estado.species}</li>)}
+      </ul>
+        </div>
+    );
 }
 
 export default MiApi
