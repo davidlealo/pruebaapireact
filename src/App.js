818259fc73aa2f231756287c9b6d8ebe1d3b3340
davidlealo <davidlealo@gmail.com>
2022-11-15 23:17:14 -0300

    HIce unos estilos a la app

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import MiApi from './components/MiApi';
 
 function App() {
   return (
-    <div className="App">
-        <MiApi />
+    <div className="App ">
+        <MiApi className="personajes"/>
+        <div className='imagenRandM'>
+        <iframe src="https://giphy.com/embed/l378BzHA5FwWFXVSg" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/adultswim-l378BzHA5FwWFXVSg">via GIPHY</a></p>
+        </div>
     </div>
   );
 }
